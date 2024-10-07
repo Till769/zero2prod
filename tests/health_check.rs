fa644e090be33498423c20f4157af0fa8d4de27e
till <till@raub>
2024-10-07 10:00:00 +0200

    Add Database for DO

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,5 @@
 //! tests/health_check.rs
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -47,17 +46,16 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create Database
-    let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create DAtabase");
 
     // Migrate Database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postges");
     sqlx::migrate!("./migrations")
@@ -89,8 +87,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let test_app = spawn_app().await;
     let configuration = get_configuration().expect("Failed to get configuration.");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string.expose_secret())
+    let mut connection = PgConnection::connect_with(&configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
