827c1ebef57ccd4e10dc0fd4edfc564a54e89e63
till <till@raub>
2024-12-05 17:09:52 +0100

    First Step Login form
    Adds a simple html login form, but with no processing of the login

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,16 @@
 //! src/routes/mod.rs
 mod error_chain_fmt;
 mod health_check;
+mod home;
+mod login;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use error_chain_fmt::*;
 pub use health_check::*;
+pub use home::*;
+pub use login::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
